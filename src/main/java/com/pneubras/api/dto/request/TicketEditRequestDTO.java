c68185f0422be6492ffac3d3d8912946abdd5e3a
RivysonClaudio <orivyson@gmail.com>
2026-04-07 17:54:20 -0300

    Enhance description field validation in TicketEditRequestDTO: Added @Size annotation to the description field, enforcing a character limit of 3 to 512 characters. This change improves input validation for ticket editing requests.

--- src/main/java/com/pneubras/api/dto/request/TicketEditRequestDTO.java
+++ src/main/java/com/pneubras/api/dto/request/TicketEditRequestDTO.java
@@ -6,5 +6,6 @@ public record TicketEditRequestDTO(
     @Size(min = 3, max = 255, message = "Title must be between 3 and 255 characters")
     String title,
 
+    @Size(min = 3, max = 512, message = "Description must be between 3 and 512 characters")
     String description
 ) {}
